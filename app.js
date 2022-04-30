7f58bf59e3be20f51c97a7825ac14f71a9ed62c1
unknown <longminhdo2000@gmail.com>
2022-05-01 00:41:52 +0700

    init prj

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+// //set the template engine ejs
+// app.set("view engine", "ejs");
+// //middlewares
+// app.use(express.static("public"));
+// //routes
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+//Listen on port 3000
+server = app.listen(3000);
